8a6f6de5efd113f1f2c0634cd294be88ec8a2d28
Josh Gerstner <jgerstner17@gmail.com>
2016-01-07 17:17:25 +0000

    Fixing Heroku

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMailer::Base
+  default to: 'jgerstner17@gmail.com'
+
+  def contact_email(name, email, body)
+    @name = name
+    @email = body
+    @body = body
+    
+    mail(from: email, subject: 'Contact Form Message')
+  end
+end
