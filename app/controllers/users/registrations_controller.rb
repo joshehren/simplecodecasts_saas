915fcc9e0a91daf1742f21f3884b385304003abb
Josh Gerstner <jgerstner17@gmail.com>
2016-01-11 19:01:33 +0000

    Modified devise for stripe

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  
   def create
     super do |resource|
       if params[:plan]
-        resource.plan_id =  params[:plan]
+        resource.plan_id = params[:plan]
         if resource.plan_id == 2
           resource.save_with_payment
         else
@@ -12,5 +11,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
     end
   end
-
 end
